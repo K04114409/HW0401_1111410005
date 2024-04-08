06338f98234a1576c6a8fb34ef5b2a98549d782d
K04114409 <155429136+K04114409@users.noreply.github.com>
2024-04-08 15:07:08 +0800

    HW0401_1111410005_黃彩瑄031
    1111410005_黃彩瑄031

---
+++ 1111410005_黃彩瑄031/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1111410005_黃彩瑄031
+{
+    internal class Program
+    {
+        static List<int> FindWordsWithChar(string[] words, char x)
+        {
+            List<int> index = new List<int>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    if (words[i][j] == x)
+                    {
+                        index.Add(i);
+                        break;
+                    }
+                }
+            }
+            return index;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] words = { "leet", "code" };
+            char x = 'e';
+            List<int> index = FindWordsWithChar(words, x);
+            for (int i = 0; i < index.Count; i++)
+            {
+                int index1 = index[i];
+                Console.WriteLine(index1);
+            }
+
+            Console.Read();
+        }
+    }
+}
+
+                
